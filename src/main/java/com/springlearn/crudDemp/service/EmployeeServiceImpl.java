e7c654d628531b9c12feeaff8935f30140c82302
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-10-17 15:50:30 +0600

    Spring boot -Data -JPA -REST -MySql -CRUD

--- src/main/java/com/springlearn/crudDemp/service/EmployeeServiceImpl.java
+++ src/main/java/com/springlearn/crudDemp/service/EmployeeServiceImpl.java
@@ -1,54 +1,54 @@
-//package com.springlearn.crudDemp.service;
+package com.springlearn.crudDemp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springlearn.crudDemp.dao.EmployeeRepository;
+import com.springlearn.crudDemp.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+	
+//	private EmployeeDao dao;
 //
-//import java.util.List;
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.springlearn.crudDemp.dao.EmployeeRepository;
-//import com.springlearn.crudDemp.entity.Employee;
-//
-//@Service
-//public class EmployeeServiceImpl implements EmployeeService{
-//	
-////	private EmployeeDao dao;
-////
-////	@Autowired
-////	public EmployeeServiceImpl(@Qualifier("employeeDaoJpaImple") EmployeeDao dao) {
-////		this.dao = dao;
-////	}
-//	
-//	private EmployeeRepository employeeRepositoryDao;
-//	
 //	@Autowired
-//	public EmployeeServiceImpl(EmployeeRepository employeeRepositoryDao) {
-//		this.employeeRepositoryDao = employeeRepositoryDao;
+//	public EmployeeServiceImpl(@Qualifier("employeeDaoJpaImple") EmployeeDao dao) {
+//		this.dao = dao;
 //	}
-//
-//	public List<Employee> findAll() {
-//		return employeeRepositoryDao.findAll();
-//	}
-//
-//	public Employee findById(int id) {
-//		Optional<Employee> result = employeeRepositoryDao.findById(id);
-//		
-//		Employee thEmployee = null;
-//		
-//		if(result.isPresent()) {
-//			thEmployee = result.get();
-//		}
-//		
-//		return thEmployee;
-//	}
-//
-//	public void save(Employee thEmployee) {
-//		employeeRepositoryDao.save(thEmployee);
-//		
-//	}
-//
-//	public void deleteById(int id) {
-//		employeeRepositoryDao.deleteById(id);
-//	}
-//
-//}
+	
+	private EmployeeRepository employeeRepositoryDao;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository employeeRepositoryDao) {
+		this.employeeRepositoryDao = employeeRepositoryDao;
+	}
+
+	public List<Employee> findAll() {
+		return employeeRepositoryDao.findAll();
+	}
+
+	public Employee findById(int id) {
+		Optional<Employee> result = employeeRepositoryDao.findById(id);
+		
+		Employee thEmployee = null;
+		
+		if(result.isPresent()) {
+			thEmployee = result.get();
+		}
+		
+		return thEmployee;
+	}
+
+	public void save(Employee thEmployee) {
+		employeeRepositoryDao.save(thEmployee);
+		
+	}
+
+	public void deleteById(int id) {
+		employeeRepositoryDao.deleteById(id);
+	}
+
+}
